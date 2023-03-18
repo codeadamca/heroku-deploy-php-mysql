c7a3c68eb54fa71695f673d43387bcee97b90a97
Adam Thomas <thomasadam83@hotmail.com>
2023-03-18 17:36:49 -0400

    Testing connection

--- import.php
+++ import.php
@@ -1,6 +1,18 @@
 <?php
 
-include('connect.php');
+// include('connect.php');
+
+echo getenv('DB_HOST');
+echo '<br>';
+echo getenv('DB_USER');
+echo '<br>';
+echo getenv('DB_PASSWORD'); 
+echo '<br>';
+echo getenv('DB_DATABASE');
+echo '<br>';
+echo function_exists('mysqli_connect');
+echo '<br>';
+
 
 $links = file_get_contents('links.sql');
 
